d95d5e0a707620507ebfe268f958e0359f10803d
Leo Espinal-Terrero <leo.espinal420@gmail.com>
2025-02-05 15:07:06 -0500

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Leo Espinal on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
